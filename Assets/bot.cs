419555920a926f127186a96aaae6662aa3093e16
sundarFRS <sundar@frslabs.com>
2022-09-01 11:41:49 +0530

    Unity files for WRO simulation

---
+++ Assets/bot.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class bot: MonoBehaviour
+{
+
+    public float Speed;
+    public Text main_x;
+    public Text main_y;
+    public Text main_z;
+    public Text aim_x;
+    public Text aim_y;
+    public Text aim_z;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (Input.GetKey("i"))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * Speed, Space.Self);
+            angle_det();
+        }
+
+        if (Input.GetKey("k"))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * -Speed, Space.Self);
+            angle_det();
+        }
+
+        if (Input.GetKey("j"))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * -Speed, Space.Self);
+            angle_det();
+        }
+
+        if (Input.GetKey("l"))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * Speed, Space.Self);
+            angle_det();
+        }
+        if (Input.GetKey("u"))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * -Speed, Space.Self);
+            angle_det();
+        }
+
+        if (Input.GetKey("o"))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * Speed, Space.Self);
+            angle_det();
+        }
+
+    }
+void angle_det()
+    {
+        
+        main_x.text = "Main X" + (int)System.Math.Ceiling(transform.eulerAngles.x);
+        main_y.text = "Main Y" + (int)System.Math.Ceiling(transform.eulerAngles.y);
+        main_z.text = "Main Z" + (int)System.Math.Ceiling(transform.eulerAngles.z);
+    }
+}
---
+++ Assets/camera.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class camera : MonoBehaviour
+{
+
+    public float Speed;
+
+    public Text main_x;
+    public Text main_y;
+    public Text main_z;
+    public Text aim_x;
+    public Text aim_y;
+    public Text aim_z;
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        angle_dir();
+        if (Input.GetKey("x"))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * Speed, Space.Self);
+            angle_dir();
+        }
+
+        if (Input.GetKey("c"))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * -Speed, Space.Self);
+            angle_dir();
+        }
+        if (Input.GetKey("q"))
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * Speed, Space.Self);
+            angle_dir();
+        }
+        if (Input.GetKey("e"))
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * -Speed, Space.Self);
+            angle_dir();
+        }
+        if (Input.GetKey("w"))
+        {
+            transform.Rotate(Vector3.left * Time.deltaTime * Speed, Space.Self);
+            angle_dir();
+        }
+        
+        if (Input.GetKey("s"))
+        {
+            transform.Rotate(Vector3.right * Time.deltaTime * Speed, Space.Self);
+
+            angle_dir();
+        }
+
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(Vector3.down * Time.deltaTime * Speed, Space.Self);
+            angle_dir();
+        }
+
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * Speed, Space.Self);
+            angle_dir();
+        }
+        
+    }
+void angle_dir()
+    {
+        aim_x.text = "arm X" + (int)System.Math.Ceiling(transform.eulerAngles.x);
+        aim_y.text = "arm Y" + (int)System.Math.Ceiling(transform.eulerAngles.y);
+        aim_z.text = "arm Z" + (int)System.Math.Ceiling(transform.eulerAngles.z);
+    }
+}
